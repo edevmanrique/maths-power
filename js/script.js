57696671a1be320998321a348489105b16fbaa5e
Edison <edisonthomas1503@gmail.com>
2021-08-03 00:41:15 -0600

    Square ready

--- js/script.js
+++ js/script.js
@@ -2,37 +2,32 @@ const PI = Math.PI;
 
 //Functions to calculate perimeters with square, triangle and circle.
 
-function s_perimeter(b, h){
-    const PERIMETER = (2 * b) + (2 * h);
-    return `The perimeter of the square is ${PERIMETER}cm`;
-}
+const s_perimeter = (b, h) => (2 * b) + (2 * h)
 
-function t_perimeter(b, s1, s2){
-    const PERIMETER = b + s1 + s2;
-    return `The perimeter of the triangle is ${PERIMETER}cm`;
-}
+const t_perimeter = (b, s1, s2) => b + s1 + s2;
 
-function c_perimeter(r){
-    const PERIMETER = (r*2) * PI;
-    return `The circumference of the circle is ${PERIMETER}cm`;
-}
+const c_perimeter = (r) => (r*2) * PI;
+
 
 //Functions to calculate area with square, triangle and circle.
 
-function s_area(b, h){
-    const AREA = b * h;
-    return `The area of the square is ${AREA}cm²`;
-}
+const s_area = (b, h) => b * h;
 
-function t_area(b, h){
-    const AREA = (b * h) / 2;
-    return `The area of the triangle is ${AREA}cm²`;
-}
+const t_area = (b, h) => (b * h) / 2;
 
-function c_area(r){
-    const AREA = (r * r) * PI;
-    return `The area of the circle is ${AREA}cm²`;
-}
+const c_area = (r) => (r * r) * PI;
 
+//Functions to receive HTML values
 
-
+const square = (op) => {
+    switch(op){
+        case 1:
+            side = parseInt(document.getElementById("square_input").value);
+            document.getElementById("square_output").value = s_perimeter(side, side) + "cm";
+        break;
+        case 2:
+            side = parseInt(document.getElementById("square_input").value);
+            document.getElementById("square_output").value = s_area(side, side) + "cm²";
+        break;
+    }
+};
